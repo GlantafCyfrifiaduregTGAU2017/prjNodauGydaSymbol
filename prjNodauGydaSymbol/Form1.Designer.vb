f15ab9b5f9f2a51969c13d50deea9a8a09e05456
hywmarsh <hywmarsh@gmail.com>
2016-06-07 22:25:25 +0100

    Trosi nodau anarferol i nodau arferol, ar ol trosi i Brifllythyren

---
+++ prjNodauGydaSymbol/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblAllbwn = New System.Windows.Forms.Label()
+        Me.btnTrosi = New System.Windows.Forms.Button()
+        Me.txtNod = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lblAllbwn
+        '
+        Me.lblAllbwn.AutoSize = True
+        Me.lblAllbwn.Location = New System.Drawing.Point(25, 131)
+        Me.lblAllbwn.Name = "lblAllbwn"
+        Me.lblAllbwn.Size = New System.Drawing.Size(39, 13)
+        Me.lblAllbwn.TabIndex = 0
+        Me.lblAllbwn.Text = "Label1"
+        '
+        'btnTrosi
+        '
+        Me.btnTrosi.Location = New System.Drawing.Point(191, 63)
+        Me.btnTrosi.Name = "btnTrosi"
+        Me.btnTrosi.Size = New System.Drawing.Size(71, 40)
+        Me.btnTrosi.TabIndex = 1
+        Me.btnTrosi.Text = "Trosi"
+        Me.btnTrosi.UseVisualStyleBackColor = True
+        '
+        'txtNod
+        '
+        Me.txtNod.Location = New System.Drawing.Point(24, 67)
+        Me.txtNod.Name = "txtNod"
+        Me.txtNod.Size = New System.Drawing.Size(117, 20)
+        Me.txtNod.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.txtNod)
+        Me.Controls.Add(Me.btnTrosi)
+        Me.Controls.Add(Me.lblAllbwn)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblAllbwn As System.Windows.Forms.Label
+    Friend WithEvents btnTrosi As System.Windows.Forms.Button
+    Friend WithEvents txtNod As System.Windows.Forms.TextBox
+
+End Class
---
+++ prjNodauGydaSymbol/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+
+    Private Sub btnTrosi_Click(sender As Object, e As EventArgs) Handles btnTrosi.Click
+        'Trosi Nod Gwreiddiol i nod arferol
+        Dim NodGwreiddiol As String
+        Dim TrosiIBrif As String
+        Dim NodArferol As String
+
+        'Darllen mewnbwn 
+        NodGwreiddiol = txtNod.Text
+        TrosiIBrif = NodGwreiddiol.ToUpper
+
+        'Newid i llythyren normal
+        Select Case AscW(TrosiIBrif)
+            Case 192 To 197
+                NodArferol = "A"
+            Case 199
+                NodArferol = "C"
+            Case 200 To 203
+                NodArferol = "E"
+            Case 204 To 207
+                NodArferol = "I"
+            Case 209
+                NodArferol = "N"
+            Case 210 To 214
+                NodArferol = "O"
+            Case 217 To 220
+                NodArferol = "U"
+            Case 221
+                NodArferol = "Y"
+            Case Else
+                NodArferol = ("Nid yw'n nod anarferol")
+        End Select
+
+
+        lblAllbwn.Text = NodArferol
+
+    End Sub
+End Class
